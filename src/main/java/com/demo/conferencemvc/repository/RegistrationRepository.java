18adca2c702aeeba0d7b73310e587b8fbc5d6bc6
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-07-10 15:38:05 +0800

    Refactored to JPA

--- src/main/java/com/demo/conferencemvc/repository/RegistrationRepository.java
+++ src/main/java/com/demo/conferencemvc/repository/RegistrationRepository.java
@@ -2,13 +2,12 @@ package com.demo.conferencemvc.repository;
 
 import com.demo.conferencemvc.model.Registration;
 import com.demo.conferencemvc.model.RegistrationReport;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RegistrationRepository {
-    Registration save(Registration registration);
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
-    List<Registration> findAll();
+    List<RegistrationReport> registrationReport();
 
-    List<RegistrationReport> findAllReports();
 }
