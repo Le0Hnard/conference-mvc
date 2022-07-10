dff95f5fc74cf9524e863427b081cc746935e017
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-07-10 15:44:48 +0800

    Updated repository

--- src/main/java/com/demo/conferencemvc/controller/RegistrationController.java
+++ src/main/java/com/demo/conferencemvc/controller/RegistrationController.java
@@ -51,5 +51,9 @@ public class RegistrationController {
         System.out.println("Registration: " + registration.getName());
         return "redirect:registration";
     }
+    @PostMapping("registration/update")
+    public @ResponseBody Registration updateRegistration(@Valid @ModelAttribute("registration") Registration registration, BindingResult result) {
+        return registrationService.addRegistration(registration);
+    }
 
 }
--- src/main/java/com/demo/conferencemvc/service/RegistrationServiceImpl.java
+++ src/main/java/com/demo/conferencemvc/service/RegistrationServiceImpl.java
@@ -25,12 +25,14 @@ public class RegistrationServiceImpl implements RegistrationService {
     public Registration addRegistration(Registration registration) {
         registration = registrationRepository.save(registration);
 
-        Course course = new Course();
-        course.setName("Intro");
-        course.setDescription("Every attendee must complete the intro");
-        course.setRegistration(registration);
+        if(registration.getId() == null) {
+            Course course = new Course();
+            course.setName("Intro");
+            course.setDescription("Every attendee must complete the intro");
+            course.setRegistration(registration);
 
-        courseRepository.save(course);
+            courseRepository.save(course);
+        }
 
         return registration;
     }
